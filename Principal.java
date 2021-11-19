061af4493713ac12d48b72042efad2bc618aeb7a
ARS-TECNOLOGIC <anselmoroberto003@gmail.com>
2021-11-19 14:45:11 -0300

    Atualizado ARS

--- Principal.java
+++ Principal.java
@@ -10,37 +10,50 @@ public class Principal {
     }
 
     public static void main(String[] args) {
-        System.out.print("\033[H\033[2J");  
-        System.out.flush(); 
-        String[] options = {
-            "1  - Cadastro Pessoa Física",
-            "2  - Cadastro Pessoal Jurídica", 
-            "3  - Cadastro Professor",  
-            "4  - Cadastro Fornecedor",  
-            "5  - Cadastro Aluno", 
-            "6  - Consulta Pessoa Física",
-            "7  - Consulta Pessoal Jurídica", 
-            "8  - Consulta Professor",  
-            "9  - Consulta Fornecedor",  
-            "10 - Consulta Aluno", 
-            "0  - Sair"
-        };
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+        String[] options = { "1  - Cadastro Pessoa Física", "2  - Cadastro Pessoal Jurídica", "3  - Cadastro Professor",
+                "4  - Cadastro Fornecedor", "5  - Cadastro Aluno", "6  - Consulta Pessoa Física",
+                "7  - Consulta Pessoal Jurídica", "8  - Consulta Professor", "9  - Consulta Fornecedor",
+                "10 - Consulta Aluno", "0  - Sair" };
         Scanner scanner = new Scanner(System.in);
         int option = 50;
-        while (option!=0) {
+        while (option != 0) {
             System.out.println("*************** Seja Bem Vindo!!! ***************");
             switch (option) {
-                case 1: optCadastroPF(); break;
-                case 2: optCadastroPJ(); break;
-                case 3: optCadastroPfs(); break;
-                case 4: optCadastroFncd(); break;
-                case 5: optCadastroAln(); break;
-                case 6: optConsultaPF(); break;
-                case 7: optConsultaPJ(); break;
-                case 8: optConsultaPfs(); break;
-                case 9: optConsultaFncd(); break;
-                case 10: optConsultaAln(); break;
-                case 50: printMenu(options); break;
+            case 1:
+                optCadastroPF();
+                break;
+            case 2:
+                optCadastroPJ();
+                break;
+            case 3:
+                optCadastroPfs();
+                break;
+            case 4:
+                optCadastroFncd();
+                break;
+            case 5:
+                optCadastroAln();
+                break;
+            case 6:
+                optConsultaPF();
+                break;
+            case 7:
+                optConsultaPJ();
+                break;
+            case 8:
+                optConsultaPfs();
+                break;
+            case 9:
+                optConsultaFncd();
+                break;
+            case 10:
+                optConsultaAln();
+                break;
+            case 50:
+                printMenu(options);
+                break;
             }
             try {
                 option = scanner.nextInt();
@@ -54,36 +67,45 @@ public class Principal {
         }
     }
 
-    //Opcoes
+    // Opcoes
     private static void optCadastroPF() {
-        System.out.println("Escolheu a opção CADASTRO");
+        System.out.println("Escolheu a opção CADASTRO!");
     }
+
     private static void optCadastroPJ() {
         System.out.println("Escolheu a opção CONSULTA");
     }
+
     private static void optCadastroPfs() {
         System.out.println("Escolheu a opção CONSULTA");
     }
+
     private static void optCadastroFncd() {
         System.out.println("Escolheu a opção CONSULTA");
     }
+
     private static void optCadastroAln() {
         System.out.println("Escolheu a opção CONSULTA");
     }
+
     private static void optConsultaPF() {
         System.out.println("Escolheu a opção CONSULTA");
     }
+
     private static void optConsultaPJ() {
         System.out.println("Escolheu a opção CONSULTA");
     }
+
     private static void optConsultaPfs() {
         System.out.println("Escolheu a opção CONSULTA");
     }
+
     private static void optConsultaFncd() {
         System.out.println("Escolheu a opção CONSULTA");
     }
+
     private static void optConsultaAln() {
         System.out.println("Escolheu a opção CONSULTA");
     }
-    
+
 }
